6a05181b5b17f135a11aaa7b215c3f605b2e55d2
ma0621450 <ma0621450@gmail.com>
2025-04-15 20:01:05 +0500

    testing query

--- app/Livewire/User/PatientHistory.php
+++ app/Livewire/User/PatientHistory.php
@@ -82,7 +82,7 @@ class PatientHistory extends Component
         if ($product->categories()->where('term_id', $this->pet_cat_id)->first() != null || $isPetSubaccount)  {
             $this->content = PatientPets::where('patient_id', $user->ID)->first();
             if($this->content == null) {
-                return redirect()->route('patient-pet', ['data' => base64_encode($user->ID)]);
+                return redirect()->route('PatientPet', ['data' => base64_encode($user->ID)]);
             }
             $this->is_pet = true;
             $this->toast(
