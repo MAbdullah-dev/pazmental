fc0e3362fc3b5b9de24757cf7d773cd340b85070
ma0621450 <ma0621450@gmail.com>
2025-04-17 22:24:27 +0500

    logging

--- app/Livewire/WizardForm.php
+++ app/Livewire/WizardForm.php
@@ -226,12 +226,15 @@ class WizardForm extends Component
             $data['images'] = $fileImages;
         }
 
-        // dd($data);
+
 
         if ($this->patient_id != '') {
             PatientDetails::where('patient_id', $this->patient_id)->update($data);
+            // Log::info('Update result:', ['result' => $result]);
+            Log::info('updating PatientDetails data:', $data);
         } else {
             PatientDetails::create($data);
+            Log::info('creating PatientDetails data:', $data);
         }
 
         return redirect('pd/' . $this->redirectionRoute);
